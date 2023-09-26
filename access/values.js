51e53bee8c8c0ff77f5b80cfdebf72afd9234dbc
radianceaqq <130775036+radianceaqq@users.noreply.github.com>
2023-09-26 22:52:02 +0300

    Add files via upload

---
+++ access/values.js
@@ -0,0 +1,24 @@
+// Активировать документы (true - включены, false - отключены)
+var isRightsEnabled = false; // Права
+var isCovidCertificateEnabled = false; // COVID-сертификат
+
+// Общее
+var birthdate = "06.05.2005"; // Дата рождения
+var fullname = "Стахова Валерія Юріївна" // ФИО
+var name = "Валерія" // Имя
+
+// Паспорт
+var passport_id = "005477036" // Номер паспорта
+
+// КПП
+var kpp_id = "0345856795" // ИНН
+
+// Права
+var rights_valid_until = "01.01.2022" // Права "Дійсні до"
+var rights_categories = "A, B" // Права "Категорії"
+var rights_tsc = "ТСЦ 8631" // Права "Видав
+var rights_id = "KBE968639" // Права "Серія та номер""
+
+// COVID-сертификат
+var covid_valid_until = "01.01.2022" // Сертификат "Дійсний до"
+var covid_certificate_id = "URN:UVCI:01:UA:0<br>E55669376876888<br>B9E2520C4F88930" // Номер сертификата (<br> - перенос строки)
